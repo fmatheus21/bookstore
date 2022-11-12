5cf3e3b9704d80b3fcc84c811f465890000e7b55
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-11-12 20:18:11 -0300

    first commit

---
+++ cambium-service/src/main/java/com/fmatheus/app/CambiumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fmatheus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambiumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambiumServiceApplication.class, args);
+	}
+
+}
